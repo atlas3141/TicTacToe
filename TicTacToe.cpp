739781b79f2a399b76f86491622479d1c6c397e6
= <atlas3141@gmail.com>
2016-09-10 18:25:24 -0700

    The before and after the main loop, and a clever way to do the turn swapping

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -3,28 +3,39 @@
 using namespace std;
 
 void printBoard(char (*board)[3]);
-void resetBoard(char (*board)[3]);
-bool checkwin(char (*board)[3], char player);
+void resetBoard(char (*board)[3], char & turn);
+bool checkWin(char (*board)[3], char player);
 bool checkTie(char (*board)[3]);
 
 int main(){
-  //initial set up
-  //playing game loop
-  //if the game hasnt reached an end state loop
-  //take in input
+  char turn = 'O'; //initial set up
+  char board[3][3];
+  char input[6];
+  bool playing = true;
+  while (playing = true){ //playing game loop
+    resetBoard(board, turn);
+    while(!(checkWin(board,turn) && checkTie(board))){ //the game hasnt reached an end state loop
+      if (turn == 'X') //swap the turns first so variables line up
+	turn = 'O';
+      else
+	turn = 'X';
+ //take in input
   //set the array
   //print the array
-  //end game loop
-  //Say who won/tied
-  //end playing loop
+      }
+    if (checkTie(board)) //say who won
+	cout << "The game ended in a tie. You both loose!" << endl;
+    else
+      cout << turn << "Wins!" << endl;
+  }
 }
 void printBoard(char (*board)[3]){
 
 }
-void resetBoard(char (*board)[3]){
+void resetBoard(char (*board)[3], char & turn){
 
 }
-bool checkwin(char (*board)[3], char player){
+bool checkWin(char (*board)[3], char player){
 
 }
 bool checkTie(char (*board)[3]){
