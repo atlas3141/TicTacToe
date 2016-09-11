90c3b35dada7a7406f83e5cc99b2444661eb0cc5
= <atlas3141@gmail.com>
2016-09-10 18:03:14 -0700

    Put in the methods and playing

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 
-using namespace std
+using namespace std;
+
+void printBoard(char (*board)[3]);
+void resetBoard(char (*board)[3]);
+bool checkwin(char (*board)[3], char player);
+bool checkTie(char (*board)[3]);
 
 int main(){
+  //initial set up
+  //playing game loop
+  //if the game hasnt reached an end state loop
+  //take in input
+  //set the array
+  //print the array
+  //end game loop
+  //Say who won/tied
+  //end playing loop
+}
+void printBoard(char (*board)[3]){
+
+}
+void resetBoard(char (*board)[3]){
+
+}
+bool checkwin(char (*board)[3], char player){
+
+}
+bool checkTie(char (*board)[3]){
 
 }
