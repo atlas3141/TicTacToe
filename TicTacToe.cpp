9c20577b59cbf3c56309aa224af9f0df3ffcebe9
= <atlas3141@gmail.com>
2016-09-10 18:38:30 -0700

    All the methods should work

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -14,6 +14,7 @@ int main(){
   bool playing = true;
   while (playing = true){ //playing game loop
     resetBoard(board, turn);
+    printBoard(board);
     while(!(checkWin(board,turn) && checkTie(board))){ //the game hasnt reached an end state loop
       if (turn == 'X') //swap the turns first so variables line up
 	turn = 'O';
@@ -30,14 +31,45 @@ int main(){
   }
 }
 void printBoard(char (*board)[3]){
-
+  cout << " \tA\tB\tC" << endl;
+  cout << "1\t" << board[0][0] << "\t" << board[1][0] << "\t" << board[2][0] << endl;
+  cout << "2\t" << board[0][1] << "\t" << board[1][1] << "\t" << board[2][1] << endl;
+  cout << "3\t" << board[0][2] << "\t" << board[1][2] << "\t" << board[2][2] << endl;
 }
 void resetBoard(char (*board)[3], char & turn){
-
+  for (int i = 0; i < 3; i++){
+    for (int j = 0; j < 3; j++){
+      board[i][j] = ' ';
+    }
+  }
+  turn = 'O';
 }
 bool checkWin(char (*board)[3], char player){
-
+  if (board[0][0] == player && board[1][1] == player && board[2][2] == player){
+    return true;
+  }
+  if (board[0][2] == player && board[1][1] == player && board[2][0] == player){
+    return true;
+  }
+  for (int count = 0; count < 3; count++) {
+    if (board[0][count] == player && board[1][count] == player && board[2][count] == player){
+      return true;
+    }
+  }
+  for (int count = 0; count < 3; count++) {
+    if (board[count][0] == player && board[count][1] == player && board[count][2] == player){
+      return true;
+    }
+  }
+  return false;
 }
 bool checkTie(char (*board)[3]){
-
+  bool tie = true; 
+  for (int i = 0; i < 3; i++){
+    for (int j = 0; j < 3; j++){
+      if (board[i][j] == ' ')
+	tie = false;
+    }
+  }
+  return tie;
 }
